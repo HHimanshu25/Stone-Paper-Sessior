b77e2c8ee5c567e6de7a221a170853154b802f3c
HHimanshu25 <himanshhukumawat2031@gmail.com>
2025-12-05 10:53:07 +0530

    update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let main1 = document.querySelector('.select1')
 let main2 = document.querySelector('.select2')
+
 window.addEventListener('load', () => {
     for (let i = 1; i <= 3; i++) {
         main1.children[`${i - 1}`].classList.toggle(`item${i}`)
     }
+    
 })
 
 main1.querySelector('.item1').addEventListener('click', () => {
@@ -36,69 +38,73 @@ function game() {
         main2.children[2].classList.toggle('item3')
     }
 }
-function reload(){
-      for (let i = 1; i <= 3; i++) {
+function reload() {
+    for (let i = 1; i <= 3; i++) {
         if (main1.children[`${i - 1}`].classList.toggle(`item${i}`))
             main1.children[`${i - 1}`].classList.toggle(`item${i}`)
 
         if (!main2.children[`${i - 1}`].classList.toggle(`item${i}`))
             main2.children[`${i - 1}`].classList.toggle(`item${i}`)
 
+        document.querySelector('.result').innerText = " "
+
     }
 }
 async function winning(win) {
     let winner = Number(win)
-    console.log("random = ", random);
-    console.log("winner = ", winner);
-
-    await new Promise(resolve => setTimeout(resolve , 500))
-    if (random == 1 && winner == 3) {
-        alert('computer is winner')
-    }
-    else if (random == 2 && winner == 1){
-
-        alert('Computer is winner')
-    }
-
-    else if (random == 3 && winner == 2){
-
-        alert('Computer is winner')
-    }
-    else if (random == 3 && winner == 1){
-
-        alert('Player  is winner')
-    }
-
-    else if (random == 1 && winner == 2){
-
-        alert('Player is winner')
-    }
-
-    else if (random == 2 && winner == 3){
-
-        alert('Player is winner')
-    }
-
-    else if (random == 3 && winner == 2){
-
-        alert('Player is winner')
-    }
-
-    else if (random == 2 && winner == 2){
-
-        alert('Match is tie')
-    }
-
-    else if (random == 3 && winner == 3){
-
-        alert('Match is tie')
-    }
     
-    else if (random == 1 && winner == 1){
-
-        alert('Match is tie')
+    if(winner == random){
+        game()
     }
-await new Promise(res => setTimeout(res , 500))
+    let result = document.querySelector('.result')
+
+    await new Promise(resolve => setTimeout(resolve, 500))
+    if (random == 1 && winner == 3) {
+        result.innerText = "Computer is winner"
+
+    }
+    else if (random == 2 && winner == 1) {
+        result.innerText = "Computer is winner"
+    }
+
+    else if (random == 3 && winner == 2) {
+        result.innerText = "Computer is winner"
+    }
+
+    else if (random == 3 && winner == 1) {
+        result.innerText = "Computer is winner"
+    }
+
+    else if (random == 1 && winner == 2) {
+
+        result.innerText = "Player is winner"
+    }
+
+    else if (random == 2 && winner == 3) {
+
+        result.innerText = "Player is winner"
+    }
+
+    else if (random == 3 && winner == 2) {
+
+        result.innerText = "Player is winner"
+    }
+
+    else if (random == 2 && winner == 2) {
+
+        result.innerText ="Match is tie"
+    }
+
+    else if (random == 3 && winner == 3) {
+
+        result.innerText ="Match is tie"
+    }
+
+    else if (random == 1 && winner == 1) {
+
+        result.innerText ="Match is tie"
+    }
+    await new Promise(res => setTimeout(res, 1000))
     reload()
 }
 
