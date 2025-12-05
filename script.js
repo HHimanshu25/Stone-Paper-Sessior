4aeb9d385d1af088d712e38db82cfd3eebf2546c
HHimanshu25 <himanshhukumawat2031@gmail.com>
2025-12-05 17:28:01 +0530

    update

--- script.js
+++ script.js
@@ -5,36 +5,37 @@ window.addEventListener('load', () => {
     for (let i = 1; i <= 3; i++) {
         main1.children[`${i - 1}`].classList.toggle(`item${i}`)
     }
-    
+
 })
 
 main1.querySelector('.item1').addEventListener('click', () => {
     main1.children[1].classList.toggle(`item2`)
     main1.children[2].classList.toggle(`item3`)
-    game()
+
 })
 main1.querySelector('.item2').addEventListener('click', () => {
     main1.children[2].classList.toggle(`item3`)
     main1.children[0].classList.toggle(`item1`)
-    game()
+
 })
 main1.querySelector('.item3').addEventListener('click', () => {
     main1.children[0].classList.toggle(`item1`)
     main1.children[1].classList.toggle(`item2`)
-    game()
+
 
 })
-let random
-function game() {
-    random = Math.floor(Math.random() * (3)) + 1;
 
-    if (random == 1) {
+let random = () =>{
+     return Math.floor(Math.random() * (3)) + 1;
+}
+function game(value) {
+    if (value == 1) {
         main2.children[0].classList.toggle('item1')
     }
-    else if (random == 2) {
+    else if (value == 2) {
         main2.children[1].classList.toggle('item2')
     }
-    else if (random == 3) {
+    else if (value == 3) {
         main2.children[2].classList.toggle('item3')
     }
 }
@@ -50,9 +51,19 @@ function reload() {
 
     }
 }
-async function winning(win) {
+async function winning(win) {    
     let winner = Number(win)    
-   
+    let rand = random()
+        
+    if(winner == rand){
+        if(rand == 3)
+            game(rand-1)
+        else
+            game(rand+1)
+    } 
+    else
+        game(rand)           
+    
     let result = document.querySelector('.result')
 
     await new Promise(resolve => setTimeout(resolve, 500))
@@ -89,17 +100,17 @@ async function winning(win) {
 
     else if (random == 2 && winner == 2) {
 
-        result.innerText ="Match is tie"
+        result.innerText = "Match is tie"
     }
 
     else if (random == 3 && winner == 3) {
 
-        result.innerText ="Match is tie"
+        result.innerText = "Match is tie"
     }
 
     else if (random == 1 && winner == 1) {
 
-        result.innerText ="Match is tie"
+        result.innerText = "Match is tie"
     }
     await new Promise(res => setTimeout(res, 1000))
     reload()
